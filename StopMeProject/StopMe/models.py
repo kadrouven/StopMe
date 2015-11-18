f3de98003a404d8c29b13f1b320b78851aaa8ef1
Callum <callumcee@gmail.com>
2015-11-18 13:04:00 +0000

    Updated models.py
    Also added a ‘via’ option

--- StopMeProject/StopMe/models.py
+++ StopMeProject/StopMe/models.py
@@ -1,9 +1,26 @@
 from django.db import models
 
-class Stations(models.Model):
+class Station(models.Model):
     stationName = models.CharField(max_length=128, unique=True)
     lat = models.CharField(max_length=128)
     lng = models.CharField(max_length=128)
-	
+
     def __unicode__(self):
         return self.stationName
+
+class Route(models.Model):
+
+    origin = models.CharField(max_length = 128)
+    destination = models.CharField(max_length = 128)
+    via = models.CharField(max_length = 128)
+
+    def __unicode__(self):
+    	return origin+' to '+destination+' via '+via #could change this string output
+
+
+class RouteStations(models.Model):
+    routeID = models.ForeignKey(Route)
+    stationID = models.ForeignKey(Station)
+
+    def __unicode__(self):
+    	return routeID+stationID #could change this string output
