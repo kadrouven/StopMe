895744197c916e4eb9f1f4cc29908d497868fe07
kadrouven <ka.drouven@gmail.com>
2015-11-23 21:20:59 +0000

    added quick search for all stations

--- StopMeProject/StopMe/views.py
+++ StopMeProject/StopMe/views.py
@@ -6,12 +6,8 @@ from StopMe.models import Station, Route, RouteStation
 def index(request):
     context = RequestContext(request)
     context_dict = {}
-    station = Station.objects.filter(lat__range=(55.868030,55.870230), type=" Train")
-    context_dict["station"] = station
-    route = Route.objects.all()
-    context_dict["route"] = route
-    routestations = RouteStation.objects.all()
-    context_dict["routestations"] = routestations
+    stations = Station.objects.all().order_by('stationName')
+    context_dict["stations"] = stations
     return render_to_response('StopMe/index.html', context_dict, context)
 
 def search(request):
@@ -23,19 +19,19 @@ def search(request):
     
     if request.POST.get("type", ""):
         context_dict["selected_type"] = request.POST.get("type", "")
-        origin = Station.objects.filter(type=(request.POST.get("type", "")))
+        origin = Station.objects.filter(type=(request.POST.get("type", ""))).order_by('stationName')
         context["origin"] = origin
         print "------", request.POST.get("type", "")
         
     if request.POST.get("origin", ""):
         context_dict["selected_origin"] = request.POST.get("origin", "")
-        route = RouteStation.objects.filter(station__stationName=request.POST.get("origin", ""))
+        route = RouteStation.objects.filter(station__stationName=request.POST.get("origin", "")).order_by('station__stationName')
         context_dict["route"] = route
     
     if request.POST.get("route", ""):
         context_dict["selected_route"] = request.POST.get("route", "")
         destination = RouteStation.objects.filter(route__destination__stationName=request.POST.get("route", ""))
-        context_dict["destination"] = destination
+        context_dict["destination"] = destination.order_by('station__stationName')
 
     return render_to_response('StopMe/search.html', context_dict, context)
 
